ac80e67992c91d7c722c2ebdacc227a9073089fa
Anais Augu <aaugu@c2r7s1.42lausanne.ch>
2023-05-17 13:50:32 +0200

    cmd_env changes to prepare unset

--- src/built-ins/cmd_env.c
+++ src/built-ins/cmd_env.c
@@ -6,20 +6,57 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 11:02:10 by aaugu             #+#    #+#             */
-/*   Updated: 2023/05/17 12:11:50 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/05/17 13:46:47 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "../../includes/minishell.h"
+#include "../../libft/libft.h"
 
-void	cmd_env(char **env)
+void	cmd_env(char **env, int env_size, char **cmd_args)
 {
 	int	i;
 
-	i = 0;
-	while (env[i])
+	if (ft_strs_len(cmd_args) == 1)
 	{
-		printf("%s\n", env[i]);
-		i++;
-	}	
+		i = 0;
+		while (env[i])
+		{
+			if (env[i])
+			{
+				printf("%s\n", env[i]);
+				i++;
+			}
+		}
+	}
+	else
+	{
+		printf("env: %s: No such file or directory\n", cmd_args[1]);
+		g_exit_code = ft_strs_len(cmd_args) - 1;
+	}
 }
+
+/*
+int	main(int ac, char **av, char **envp)
+{
+	char	**cmd_args;
+	int		i = 0;
+
+	cmd_args = (char **)malloc(sizeof(char *) * (ac));
+	if (!cmd_args)
+	{
+		printf("Malloc error\n");
+		return (1);
+	}
+	cmd_args[ac] = NULL;
+	while (i < ac)
+	{
+		cmd_args[i] = av[i + 1];
+		i++;
+	}
+	cmd_env(envp, cmd_args);
+	printf("\ng_exit_code : %d\n", g_exit_code);
+	return (0);
+}
+*/
--- src/built-ins/cmd_exit.c
+++ src/built-ins/cmd_exit.c
@@ -6,15 +6,15 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 11:02:15 by aaugu             #+#    #+#             */
-/*   Updated: 2023/05/17 12:12:04 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/05/17 12:22:03 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	cmd_exit(t_data *data, int cmd_nbr)
+void	cmd_exit(t_data *data, char **cmd_args, int cmd_nbr)
 {
-	if (cmd_nbr == 1)
+	if (cmd_nbr == 1 && ft_strs_len(cmd_args) == 1)
 	{
 		printf("exit\n");
 		clear_minishell(data);
