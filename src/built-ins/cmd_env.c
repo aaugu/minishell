7d6e6b82e191da72dabae7b727743893b5a1f620
Anais Augu <aaugu@c2r7s1.42lausanne.ch>
2023-05-17 12:12:52 +0200

    built-in cmd_env.c ok

--- src/built-ins/cmd_env.c
+++ src/built-ins/cmd_env.c
@@ -6,7 +6,20 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 11:02:10 by aaugu             #+#    #+#             */
-/*   Updated: 2023/05/17 11:02:12 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/05/17 12:11:50 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+void	cmd_env(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}	
+}
