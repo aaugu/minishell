9f1f8c3b78abc0c0800908eb4266f3deb2a628aa
Anais Augu <aaugu@c2r7s1.42lausanne.ch>
2023-05-30 09:45:32 +0200

    started changes

--- src/built-ins/cmd_exit.c
+++ src/built-ins/cmd_exit.c
@@ -6,12 +6,13 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 11:02:15 by aaugu             #+#    #+#             */
-/*   Updated: 2023/05/22 13:47:43 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/05/30 09:40:35 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "../../includes/minishell.h"
+#include "../../libft/libft.h"
 
 /*
 Exit in bash : The exit() function causes normal process termination and the
@@ -21,9 +22,18 @@ here it is
 
 int	cmd_exit(t_data *data, char **cmd_args, int cmd_nbr)
 {
-	if (cmd_nbr == 1 && ft_strs_len(cmd_args) == 1)
+	if (cmd_nbr == 1)
 	{
 		printf("exit\n");
-		clear_minishell(data);
+		if (ft_strs_len(cmd_args) == 1)
+			clear_minishell(data);
+		else if (ft_strs_len(cmd_args) == 2)
+		{
+			g_exit_code = print_err("minishell: exit: asd: numeric argument required", 255);
+		}
+		else if (ft_strs_len(cmd_args) >= 2)
+		{
+			g_exit_code = print_err("minishell: exit: too many arguments", 0);
+		}
 	}
 }
