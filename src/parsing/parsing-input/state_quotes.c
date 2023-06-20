9cb5b477808ef4f1f273b003c4458b5b50df6787
Anais Augu <aaugu@c3r2s5.42lausanne.ch>
2023-06-20 13:53:32 +0200

    double quotes empty argument

--- src/parsing/parsing-input/state_quotes.c
+++ src/parsing/parsing-input/state_quotes.c
@@ -6,7 +6,7 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 09:55:39 by aaugu             #+#    #+#             */
-/*   Updated: 2023/06/20 13:38:39 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/06/20 13:51:15 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,14 @@
 #include "parsing_meta.h"
 #include "libft.h"
 
+void	create_node_empty_string(t_fsm *fsm, t_token **tokens, char c);
+
 /* Will set current state according to char and decide which action to perform
 if needed */
 void	state_quote_s(t_fsm *fsm, t_token **tokens, char c)
 {
-	t_token	*new_token;
-	t_token	*prev;
-
 	if (c == '\'' && fsm->buf_size == 0)
-	{
-		new_token = create_node(fsm);
-		if (!new_token)
-			parsing_error(fsm, 0);
-		if (*tokens == NULL)
-			*tokens = new_token;
-		else
-		{
-			prev = lst_last(*tokens);
-			prev->next = new_token;
-			new_token->prev = prev;
-		}
-		init_state_machine(fsm);
-		get_next_type(fsm, c);
-	}
+		create_node_empty_string(fsm, tokens, c);
 	if (c == '\'')
 		change_state_quotes(fsm, idle);
 	else if (c == '\0')
@@ -51,25 +36,8 @@ void	state_quote_s(t_fsm *fsm, t_token **tokens, char c)
 if needed */
 void	state_quote_d(t_fsm *fsm, t_token **tokens, char c)
 {
-	t_token	*new_token;
-	t_token	*prev;
-
 	if (c == '\"' && fsm->buf_size == 0)
-	{
-		new_token = create_node(fsm);
-		if (!new_token)
-			parsing_error(fsm, 0);
-		if (*tokens == NULL)
-			*tokens = new_token;
-		else
-		{
-			prev = lst_last(*tokens);
-			prev->next = new_token;
-			new_token->prev = prev;
-		}
-		init_state_machine(fsm);
-		get_next_type(fsm, c);
-	}
+		create_node_empty_string(fsm, tokens, c);
 	if (c == '\"')
 		change_state_quotes(fsm, idle);
 	else if (c == '$')
@@ -78,9 +46,27 @@ void	state_quote_d(t_fsm *fsm, t_token **tokens, char c)
 		fsm->current_state = dollar_quotes;
 	}
 	else if (c == '\0')
-	{
 		parsing_error(fsm, "\"");
-	}
 	else
 		add_to_buf(fsm, c);
+}
+
+void	create_node_empty_string(t_fsm *fsm, t_token **tokens, char c)
+{
+	t_token	*new_token;
+	t_token	*prev;
+
+	new_token = create_node(fsm);
+	if (!new_token)
+		parsing_error(fsm, 0);
+	if (*tokens == NULL)
+		*tokens = new_token;
+	else
+	{
+		prev = lst_last(*tokens);
+		prev->next = new_token;
+		new_token->prev = prev;
+	}
+	init_state_machine(fsm);
+	get_next_type(fsm, c);
 }
